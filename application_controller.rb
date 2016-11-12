76b751d20a16c5e9a84471d13c3d98a55c559c6b
senyaha <emma.butler@smes.org>
2016-11-12 10:38:31 +0000

    update

--- application_controller.rb
+++ application_controller.rb
@@ -6,21 +6,9 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
-  
-  get '/about' do
-    erb :about
-  end
-  
-  post '/calculate' do
-    #getting info out of params hash
-    first=params[:num1].to_f
-    second=params[:num2].to_f
-    user_birth=params[:birthday]
-    #calculating using info from params
-    @sum=add(first,second)
-    @days_alive=years_alive(user_birth)
-    #directing to our results page
-    erb :results
+
+  get '/refugee.erb' do
+    erb :refugee
   end
 
 end
