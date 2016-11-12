976d06f40ccb69bd9d295e2b36982b85ad48b103
senyaha <emma.butler@smes.org>
2016-11-12 12:42:04 +0000

    update v3

--- application_controller.rb
+++ application_controller.rb
@@ -11,7 +11,39 @@ class MyApp < Sinatra::Base
     erb :refugee
   end
 
-  get 'zika.erb' do
+  get '/zika.erb' do
     erb :zika
+  end
+
+  get '/climate.erb' do
+    erb :climate
+  end
+
+  get '/equality.erb' do
+    erb :equality
+  end
+
+  get '/about.erb' do
+    erb :about
+  end
+
+  get '/advocate.erb' do
+    erb :advocate
+  end
+
+  get '/volunteer.erb' do
+    erb :volunteer
+  end
+
+  get '/education.erb' do
+    erb :education
+  end
+
+  get '/water.erb' do
+    erb :water
+  end
+
+  get '/equality.erb' do
+    erb :equality
   end
 end
